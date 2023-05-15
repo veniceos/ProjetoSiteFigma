fbacbeec89cec8b0c7b2a13c3de4726a161f4e0a
veniceos <vinicius.canevari@gmail.com>
2023-05-15 07:39:42 -0300

    Arquivos não finalizados.

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Island+Moments&display=swap">
+    <title>Document</title>
+</head>
+<body>
+<div class='site'>
+<header>
+    <div class='cbc'>
+    <p class='ttl'>
+    Old Millionaires
+</p>
+    <p class='ttl1'>
+        De todas as idades e patrimônios
+    </p>
+    <img src='Fotos/tio-patinhas-origem-curiosidades-e-licoes-do-pato-mais-rico-da-ficcao-e1610500057428.jpg'>
+    </img>
+    <a class='btao' href=''>Contato</a>
+    <a class='btao' href=''>Menu</a>
+    <a class='btao' href=''>Gênero</a>
+</div>
+</header>
+<section>
+</section>
+
+</div>
+</body>
+</html>
