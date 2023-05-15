cef3f837321b0f1a2840772049372784d8d8d80e
veniceos <vinicius.canevari@gmail.com>
2023-05-15 11:39:04 -0300

    Contato, Serviço e index feitos.

--- serviço.php
+++ serviço.php
@@ -1,13 +1,116 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href='style.css'>
+    <link rel="stylesheet" href="./CSS/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Island+Moments&display=swap">
     <title>Document</title>
 </head>
 <body>
+<div class='site'>
+<header>
     
+<div>
+    <p class='ttl'>
+    Old Millionaires
+</p>
+    <p class='ttl1'>
+        De todas as idades e patrimônios
+    </p>
+    <br>
+    <a class='btao' href='contato.php'>Contato</a>
+    <a class='btao' href='index.php'>Menu</a>
+    <a class='btao' href='serviços.php'>Gênero</a>
+
+</div>
+<div class="rgt">
+<img src='Fotos/tio-patinhas-origem-curiosidades-e-licoes-do-pato-mais-rico-da-ficcao-e1610500057428.jpg' class='ft'>
+</div>
+    </img>
+</table>
+</header>
+<section>
+
+<p class="ttlft">Homens</p>
+<div class="tbl">
+<table align="center" border="2">
+
+    <tr> 
+        <td>
+    
+        <div class="container1">
+    <img src="Fotos/Silvio_Santos_em_maio_de_2019.jpg" class="fto">
+    <br>Nome Completo: Senor Abravanel<br>Fortuna: R$ 1.7b<br>
+    <a href="" class="select">Match</a>
+</div></td>
+<td>
+<div class="container1">
+    <img src="Fotos/1200px-Roberto_Carlos_Credicard_Hall_(42626066250).jpg" class="fto">
+    <br>Nome Completo: Roberto Carlos Braga<br>Fortuna: U$ 200m <br>
+    <a href="" class="select">Match</a>
+</div></td>
+<td>
+<div class="container1">
+    <img src="Fotos/Faustão_2015.png" class="fto">
+    <br>Nome Completo: Fausto Correia da Silva<br>Fortuna: R$ 1.1b<br>
+    <a href="" class="select">Match</a>
+</div></td>
+<td>
+<div class="container1">
+    <img src="Fotos/2010-08-08_Leonardo_na_Expo_FISAV_04.jpg" class="fto">
+    <br>Nome Completo: Emiral Eterno da Costa<br>Fortuna: R$ 180m<br>
+    <a href="" class="select">Match</a>
+</div>
+</td>
+</table>
+</div>
+
+<p class="ttlft">Mulheres</p>
+<div class="tbl">
+<table align="center" border="2">
+
+    <tr> 
+
+    <td>
+<div class="container1">
+    <img src="Fotos/Ana_Maria_Braga.jpg" class="fto">
+    <br>Nome Completo: Ana Maria Braga Maffeis<br>Fortuna: R$ 100m<br>
+    <a href="" class="select">Match</a>
+</div></td>
+
+<td>
+<div class="container1">
+    <img src="Fotos/Fátima_Bernardes.png" class="fto">
+    <br>Nome Completo: Fátima Gomes Bernardes<br>Fortuna: R$ 15m<br>
+    <a href="" class="select">Match</a>
+</div></td>
+
+<td>
+<div class="container1">
+    <img src="Fotos/Xuxa.jpg" class="fto">
+    <br>Nome Completo: Maria da Graça Xuxa Meneghel<br>Fortuna: R$ 160m<br>
+    <a href="" class="select">Match</a>
+</div></td>
+
+<td>
+<div class="container1">
+    <img src="Fotos/maria berklian.jpg" class="fto">
+    <br>Nome Completo: Maria Berklian<br>Fortuna: Desconhecida<br>
+    <a href="" class="select">Match</a>
+</div></td>
+
+</div>
+</td>
+</table>
+</div>
+</div>
+
+
+
+
+
+  </section>
 </body>
 </html>
